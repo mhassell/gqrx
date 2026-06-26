7522f91abadb6c8da22b627dc07920819ed0d846
Matthew Hassell <hassell.matthew@gmail.com>
2026-06-26 10:45:56 -0400

    Fix shared_ptr creation using direct constructor

--- src/applications/gqrx/shm_iq_sink.cpp
+++ src/applications/gqrx/shm_iq_sink.cpp
@@ -5,5 +5,5 @@
 shm_iq_sink::sptr
 shm_iq_sink::make(unsigned long sample_rate)
 {
-    return boost::make_shared<shm_iq_sink_impl>(sample_rate);
+    return boost::shared_ptr<shm_iq_sink>(new shm_iq_sink_impl(sample_rate));
 }
