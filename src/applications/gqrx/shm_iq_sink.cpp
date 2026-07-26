d1c97409a2bacf0df14623ba4ec935fc302cb1af
Matthew Hassell <hassell.matthew@gmail.com>
2026-07-26 14:15:04 -0400

    Fix shm_iq_sink::sptr to use GNURADIO_VERSION-guarded shared_ptr, matching codebase convention (fixes std::shared_ptr<gr::basic_block> conversion error)

--- src/applications/gqrx/shm_iq_sink.cpp
+++ src/applications/gqrx/shm_iq_sink.cpp
@@ -5,5 +5,9 @@
 shm_iq_sink::sptr
 shm_iq_sink::make(unsigned long sample_rate)
 {
+#if GNURADIO_VERSION < 0x030900
     return boost::shared_ptr<shm_iq_sink>(new shm_iq_sink_impl(sample_rate));
+#else
+    return std::shared_ptr<shm_iq_sink>(new shm_iq_sink_impl(sample_rate));
+#endif
 }
--- src/applications/gqrx/shm_iq_sink.hpp
+++ src/applications/gqrx/shm_iq_sink.hpp
@@ -5,7 +5,11 @@
 
 class shm_iq_sink : virtual public gr::sync_block {
 public:
+#if GNURADIO_VERSION < 0x030900
     typedef boost::shared_ptr<shm_iq_sink> sptr;
+#else
+    typedef std::shared_ptr<shm_iq_sink> sptr;
+#endif
 
     /**
      * \brief Return a shared_ptr to a new instance of shm_iq_sink.
